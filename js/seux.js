60ac59cd01abf781bfe1167b1d221fb064399aac
Robomatix Rebirth <robomatix@robomatix.net>
2013-08-31 14:54:21 +0200

    New way of coding seux... The hero can move right and left.

--- js/seux.js
+++ js/seux.js
@@ -1,154 +1,78 @@
-var game = function() {
-			
-				// private methods/properties
-				var private = {};
+// Global constants:
+var PLAYGROUND_WIDTH	= 500;
+var PLAYGROUND_HEIGHT	= 500;
+var REFRESH_RATE		= 25;
+
+// Gloabl animation holder
+var playerAnimation = new Array();
+
+
+// Game state
+var gameOver = false;
+
+// Some hellper functions : 
+
+// Function to restart the game:
+function restartgame(){
+	window.location.reload();
+};
+
+// Game objects:
+function Player(node){
+
+	this.node = node;
+	//this.animations = animations;
+	
+	return true;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// --                                      the main declaration:                                                     --
+// --------------------------------------------------------------------------------------------------------------------
+$(function(){
+	// Animations declaration: 
+	
+	// Player space shipannimations:
+	playerAnimation["idle"]		= new $.gQ.Animation({imageURL: "images/hero.png"});
+	
+	// Initialize the game:
+	$("#playground").playground({height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH, keyTracker: true});
 				
-				private.PLAYGROUND_WIDTH = 500;
-				private.PLAYGROUND_HEIGHT = 500;
-				
-				private.status = -1;
-				
-				
-				var get = function(key) {
-					return private[key];
-				}
-				
-				var set = function(key, val) {
-					private[key] = val;
-				}
-
-				// public methods/properties
-				return {
-					init: function() {
-						
-						$("#pause").hide();
-						
-						$("#playground").playground({
-							height: get('PLAYGROUND_HEIGHT'),
-							width: get('PLAYGROUND_WIDTH'),
-							refreshRate: 30
-						});
-
-						$.playground().addSprite('heroSpaceship', {
-							animation: new $.gameQuery.Animation({
-								imageURL:"images/hero.png" }),
-							width: 40,
-							height: 40
-						});
-						
-					
-
-						this.initPlayer();
+	// Initialize the background
+	$.playground().addGroup("actors", {width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
+							.addSprite("player",{animation: playerAnimation["idle"], posx: (PLAYGROUND_WIDTH/2)-20, posy: 455, width: 40, height: 40})
+						.end()
+					.end();
+	
+	$("#player")[0].player = new Player($("#player"));
+		
+	//initialize the start button
+	$("#startbutton").click(function(){
+		$("#console").html('click');
+		$.playground().startGame(function(){
+			$("#welcomeScreen").fadeTo(1000,0,function(){$(this).remove();});
+			$("#console").html('Game Started');
+		});
+	})
+	
+	// this is the function that control most of the game logic 
+	$.playground().registerCallback(function(){
 
 
-						
-						var classObj = this;
-						$.playground().registerCallback(function() {
-
-							var status = get('status');
-
-							if (status > 0) {
-								// game is running
-							}
-						}, 30);
-						
-					},
-					initPlayer: function() {
-						var player = $('#heroSpaceship');
-						player.addClass('player');
-						player.xy(get('PLAYGROUND_WIDTH')/2-player.width(), 455 );
-			
-					},
-					renderScores: function() {
-						$('#score').text( $('#heroSpaceship').get(0).gameQuery.score );
-					},
-					movePlayer: function(player, dir){
-						if (get('status') == 1) {
-							var pos = $(player).position();
-							var newPos = pos.left + dir;
-							if (newPos > 0 && newPos+$(player).width() < get('PLAYGROUND_WIDTH')) {
-								// move the player if within gamearea
-								$(player).x(newPos);
-							}
-						}
-					},
-					keyDownHandler: function(evt) {
-						// console.log(evt);
-						var thisObj = this;
-						switch(evt.keyCode) {
-							case 13:
-								if (get('status') == -1) {
-									this.start();
-								} else {
-									this.pause();
-								}
-								break;
-							case 37:// arrow left
-							case 87:// w
-								if (! this.moveRightInt) {
-									this.moveRightInt = window.setInterval( function() { thisObj.movePlayer('#heroSpaceship', -4); }, 20);
-								}
-								break;
-							case 39:// arrow right
-							case 88:// x
-								if (! this.moveRightInt) {
-									this.moveRightInt = window.setInterval( function() { thisObj.movePlayer('#heroSpaceship', 4); }, 20);
-								}
-								break;
-						}
-					},
-					keyUpHandler: function(evt) {
-						switch(evt.keyCode) {
-							case 37:
-							case 87:
-							case 39:
-							case 88:
-								window.clearInterval(this.moveRightInt);
-								this.moveRightInt = null;
-								break;
-						}						
-						
-					},
-					restart: function() {
-						this.pause();
-						this.renderScores();
-						this.initPlayers();
-					},
-					start: function() {
-						if (get('status') == -1) {
-							set('status', 1);
-							$.playground().startGame(function(){
-								$("#welcome").remove();
-							});
-						}						
-					},
-					pause: function() {
-						var status = get('status');
-						if (status == 1) {
-							status = 0;
-							$("#pause").show();
-						} else if (status == 0) {
-							status = 1;
-							$("#pause").hide();
-						}
-						set('status', status);
+			//Update the movement of the ship:
+				if(jQuery.gameQuery.keyTracker[37] || jQuery.gameQuery.keyTracker[87]){ //this is left! (<- or w)
+					var nextpos = $("#player").x()-5;
+					if(nextpos > 0){
+						$("#player").x(nextpos);
 					}
 				}
-				
-			}();
-			
-			
-			$(function(){
-				game.init();
+				if(jQuery.gameQuery.keyTracker[39] || jQuery.gameQuery.keyTracker[88]){ //this is right! (-> or x)
+					var nextpos = $("#player").x()+5;
+					if(nextpos < PLAYGROUND_WIDTH - 40){
+						$("#player").x(nextpos);
+					}
+				}
+	}, REFRESH_RATE);
 
-				$('#welcome span').click(function(){
-					game.start();				
-				});
-				
-				$(document).keydown(function(evt){
-					game.keyDownHandler(evt);
-				});
-				$(document).keyup(function(evt){
-					game.keyUpHandler(evt);
-				});
-			});
+});
+
